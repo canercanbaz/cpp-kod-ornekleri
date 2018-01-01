8206fcc7bd77ecdd6337e869978ee926b4dfd043
Caner Canbaz <caneer@hotmail.com.tr>
2018-01-01 19:19:05 +0300

    Turkce karakter dosyasi eklendi

---
+++ TurkceKarakterCozumu.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   setlocale(LC_ALL,"Turkish"); // Türkçe karakter sorununu düzeltmek için
+   cout << "Bu bir Türkçe karakter içeren cümle örneðidir.";
+   return 0;
+} 
